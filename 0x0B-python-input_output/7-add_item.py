05360b83f21f29d0d42a790d2e4de1f44a324a6f
ttsilesh <ttsilesh87gmail.com>
2023-04-12 11:24:27 -0700

    add json files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""
- save items to a file.
-"""
-
-import sys
+"""Module for saving to json"""
+import json
 import os.path
-
-args = sys.argv[1:]
+import sys
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-item = []
-if os.path.exists("./add_item.json"):
-    item = load_from_json_file("add_item.json")
+filename = "add_item.json"
+json_list = []
 
-save_to_json_file(item + args, "add_item.json")
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
+
+for index in argv[1:]:
+    json_list.append(index)
+
+save_to_json_file(json_list, filename)
