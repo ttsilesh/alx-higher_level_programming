723733614a565863560529c8a0006cdd7c2e2036
ttsilesh <ttsilesh87gmail.com>
2023-06-30 12:55:28 -0700

    error request

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
         try:
-            with request.urlopen(sys.argv[1]) as response:
+            with urllib.error.HTTPError as response:
                 body = response.read()
                 print(body.decode('utf-8'))
         except error.HTTPError as err:
